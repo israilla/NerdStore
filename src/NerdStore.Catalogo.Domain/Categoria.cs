009e3a25727e9224c48c897fb5da01e6526cb0da
Israilla <israillacaroline@hotmail.com>
2021-12-10 13:35:21 -0300

    Criando entidades

---
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -0,0 +1,31 @@
+﻿using NerdStore.Core.DomainObjects;
+using System.Collections.Generic;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Categoria : Entity
+    {
+        public string Nome { get; private set; }
+        public int Codigo { get; private set; }
+        public ICollection<Produto> Produtos { get; set; }
+
+        protected Categoria() { }
+
+        public Categoria(string nome, int codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+
+            Validar();
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}";
+        }
+
+        public void Validar()
+        {
+        }
+    }
+}
